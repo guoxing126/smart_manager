c91fbf4b35eb707418e92be17296b366d6f7ca2a
guoxing126 <gxing126@126.com>
2018-12-10 00:09:09 +0800

    sidebar显示效果完成

--- web/urls.py
+++ web/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     url(r'^logout.html', account.logout),
     # url(r'^index.html', home.blog_index, name='index'),
     url(r'^home.html',home.device_overview),
+    url(r'^network_list.html', home.network_list),
+    url(r'^server_list.html',home.server_list),
+    url(r'^device_manage.html',home.device_manage),
+    url(r'^device_alert.html',home.device_alert),
+    url(r'^system_settings.html',home.system_settings),
     url(r'^', home.device_overview),
 ]
--- web/views/home.py
+++ web/views/home.py
@@ -1,8 +1,19 @@
+import json
+import inspect
+
 from django.shortcuts import render, redirect, HttpResponse
 from repository import models
 from utils.pagination import Pagination
 from django.urls import reverse
 
+sidebar_dcit = {
+    'device_overview':'管理概况',
+    'network_list':'网络设备列表',
+    'server_list': '服务器列表',
+    'device_manage': '设备管理',
+    'device_alert': '设备告警',
+    'system_settings': '系统设置',
+}
 
 
 def device_overview(request, *args, **kwargs):
@@ -13,7 +24,14 @@ def device_overview(request, *args, **kwargs):
     :param kwargs:
     :return:
     '''
-    return render(request, 'home.html')
+    fname = inspect.stack()[0][3]
+    sidebar_name = sidebar_dcit.get(fname)
+    context_data ={
+        'fname':fname,
+        'sidebar_name':sidebar_name,
+        'sidebar_dcit':sidebar_dcit,
+    }
+    return render(request, 'home.html', context_data)
 
 
 def network_list(request, *args, **kwargs):
@@ -24,7 +42,14 @@ def network_list(request, *args, **kwargs):
     :param kwargs:
     :return:
     '''
-    pass
+    fname = inspect.stack()[0][3]
+    sidebar_name = sidebar_dcit.get(fname)
+    context_data = {
+        'fname': fname,
+        'sidebar_name': sidebar_name,
+        'sidebar_dcit': sidebar_dcit,
+    }
+    return render(request, 'network_list.html', context_data)
 
 
 def server_list(request, *args, **kwargs):
@@ -35,7 +60,14 @@ def server_list(request, *args, **kwargs):
     :param kwargs:
     :return:
     '''
-    pass
+    fname = inspect.stack()[0][3]
+    sidebar_name = sidebar_dcit.get(fname)
+    context_data = {
+        'fname': fname,
+        'sidebar_name': sidebar_name,
+        'sidebar_dcit': sidebar_dcit,
+    }
+    return render(request, 'server_list.html', context_data)
 
 
 def device_manage(request, *args, **kwargs):
@@ -46,7 +78,14 @@ def device_manage(request, *args, **kwargs):
     :param kwargs:
     :return:
     '''
-    pass
+    fname = inspect.stack()[0][3]
+    sidebar_name = sidebar_dcit.get(fname)
+    context_data = {
+        'fname': fname,
+        'sidebar_name': sidebar_name,
+        'sidebar_dcit': sidebar_dcit,
+    }
+    return render(request, 'device_manage.html', context_data)
 
 
 def device_alert(request, *args, **kwargs):
@@ -57,7 +96,14 @@ def device_alert(request, *args, **kwargs):
     :param kwargs:
     :return:
     '''
-    pass
+    fname = inspect.stack()[0][3]
+    sidebar_name = sidebar_dcit.get(fname)
+    context_data = {
+        'fname': fname,
+        'sidebar_name': sidebar_name,
+        'sidebar_dcit': sidebar_dcit,
+    }
+    return render(request, 'device_alert.html', context_data)
 
 
 def system_settings(request, *args, **kwargs):
@@ -68,4 +114,11 @@ def system_settings(request, *args, **kwargs):
     :param kwargs:
     :return:
     '''
-    pass
+    fname = inspect.stack()[0][3]
+    sidebar_name = sidebar_dcit.get(fname)
+    context_data = {
+        'fname': fname,
+        'sidebar_name': sidebar_name,
+        'sidebar_dcit': sidebar_dcit,
+    }
+    return render(request, 'system_settings.html', context_data)
